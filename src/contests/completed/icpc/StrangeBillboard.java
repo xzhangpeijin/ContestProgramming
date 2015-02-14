28c89e6ef95815079d4a63669ef6dd9d244d4514
Peijin Zhang <xzhangpeijin@gmail.com>
2015-02-14 11:27:38 -0500

    Polish StrangeBillboard

--- src/contests/completed/icpc/StrangeBillboard.java
+++ src/contests/completed/icpc/StrangeBillboard.java
@@ -1,222 +1,316 @@
 package contests.completed.icpc;
-
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class StrangeBillboard {
-  public int safeMod(int i) {
-    i %= 2;
-    if (i < 0) {
-      i += 2;
-    }
-    return i;
-  }
-
-  public int[][] makeMatrix(int R, int C) {
-    int K = R * C;
-    int M[][] = new int[K][K];
-
-    for (int i = 0; i < R; i++) {
-      for (int j = 0; j < C; j++) {
-        int k = C * i + j;
-        M[k][k] = 1;
-        if (i > 0) {
-          M[k][k - C] = 1;
-        }
-        if (i < R - 1) {
-          M[k][k + C] = 1;
-        }
-        if (j > 0) {
-          M[k][k - 1] = 1;
-        }
-        if (j < C - 1) {
-          M[k][k + 1] = 1;
-        }
+/**
+ * Solves the strange billboard ACM problem.
+ * 
+ * Problem statement can be found here: 
+ *    https://icpcarchive.ecs.baylor.edu/external/39/3953.pdf
+ * The problem is essentially the famous "lights out" game on an arbitrary board size.
+ * 
+ * Solving the board is done by solving the system of linear equations defined by
+ * Mx = b where M is the transformation matrix defined by touches in the game, x is the set 
+ * of moves to make, and b is the final board state. Solutions are determined using 
+ * Gauss-Jordan elimination on the transformation matrix.
+ * 
+ * @author Peijin Zhang
+ */
+public class StrangeBillboard
+{   
+  /**
+   * Main solver function.
+   */
+  private void solve() throws IOException {
+    int rows, cols;
+    while ((rows = nextInt()) != 0 && (cols = nextInt()) != 0) {
+      int[] board = readBoard(rows, cols);
+      int tiles = solveBoard(rows, cols, board);
+      if (tiles == -1) {
+        out.format("Damaged billboard.%n");
+      } else {
+        out.format("You have to tap %d tiles.%n", tiles);
       }
     }
-    return M;
   }
-
-  public void swap(int v[], int i, int j) {
-    int temp = v[i];
-    v[i] = v[j];
-    v[j] = temp;
-  }
-
-  public void swapCols(int M[][], int i, int j) {
-    for (int k = 0; k < M.length; k++) {
-      int temp = M[k][i];
-      M[k][i] = M[k][j];
-      M[k][j] = temp;
-    }
-  }
-
-  public void swapRows(int M[][], int i, int j) {
-    for (int k = 0; k < M[0].length; k++) {
-      int temp = M[i][k];
-      M[i][k] = M[j][k];
-      M[j][k] = temp;
-    }
-  }
-
-  public int gauss(int M[][], int perm[], int y[]) {
-    int K = M.length;
-
-    for (int p = 0; p < K; p++) {
-      perm[p] = p;
-    }
-
-    for (int p = 0; p < K; p++) {
-      int c = p, r = p;
-      while (c < K && M[r][c] == 0) {
-        r++;
-        if (r >= K) {
-          c++;
-          r = p;
-        }
-      }
-      if (c >= K) {
-        return p;
-      }
-
-      swapRows(M, r, p);
-      swap(y, r, p);
-      swapCols(M, c, p);
-      swap(perm, c, p);
-
-      int u = M[p][p];
-      M[p][p] = 1;
-      for (int q = p + 1; q < K; q++)
-        M[p][q] = safeMod(M[p][q] * u);
-      y[p] = safeMod(y[p] * u);
-
-      for (r = p + 1; r < K; r++) {
-        int v = M[r][p];
-        if (v == 0)
-          continue;
-        M[r][p] = 0;
-        for (int q = p + 1; q < K; q++)
-          M[r][q] = safeMod(M[r][q] - v * M[p][q]);
-        y[r] = safeMod(y[r] - v * y[p]);
+  
+  /**
+   * Reads the input board into a single array in row major order.
+   *    An ON light is represented with a 1. OFF is 0.
+   * Assumes the input is well formatted, meaning that there are a total of 
+   * rows lines and each line has cols characters that are either 'X' or '.'.
+   */
+  private int[] readBoard(int rows, int cols) throws IOException {
+    int[] board = new int[rows * cols];
+    for (int i = 0; i < rows; i++) {
+      String line = nextLine();
+      for (int j = 0; j < cols; j++) {
+        board[i * cols + j] = (line.charAt(j) == 'X') ? 1 : 0;
       }
     }
-
-    return K;
+    return board;
   }
+  
+  /**
+   * Solves the given board. Return value is -1 if the board is not solvable, or
+   * the minimum number of moves required to solve the board.
+   */
+  private static int solveBoard(int rows, int cols, int[] board) {
+    int length = rows * cols;
 
-  public void backSubst(int K, int M[][], int x[], int perm[], int y[], int rank) {
-    for (int p = rank - 1; p >= 0; p--) {
-      int u = y[p];
-      for (int q = p + 1; q < K; q++) {
-        u = safeMod(u - M[p][q] * x[perm[q]]);
-      }
-      x[perm[p]] = u;
-    }
-  }
-
-  public int count(int v[]) {
-    int total = 0;
-    for (int i : v) {
-      total += i;
-    }
-    return total;
-  }
-
-  public int solve(int R, int C, int y[]) {
-    int M[][] = makeMatrix(R, C);
-    int K = R * C;
-    int perm[] = new int[K];
-    int rank = gauss(M, perm, y);
-
-    for (int i = rank; i < K; i++) {
-      if (y[i] != 0) {
+    // Create the transformation matrix.
+    int[][] transform = makeTransformMatrix(rows, cols);
+    
+    // Perform Gauss-Jordan elimination on transformation matrix.
+    gaussJordan(transform, board);
+    
+    int rank = getRank(transform);
+    
+    /* Check if our system of equations is solvable.
+     * Everything below rank in the row reduced matrix should be 0, so if board is non-zero,
+     * that means one of our transformed equations is 0 = 1, which is impossible.
+     */
+    for (int i = rank; i < length; i++) {
+      if (board[i] != 0) {
         return -1;
       }
     }
-
-    int n = 1;
-    for (int i = rank; i < K; i++) {
-      n *= 3;
+    
+    /* Our board is solvable, so check all solutions and find the least moves needed.
+     * Given x free positions, we will have 2^x possible solutions.
+     */
+    List<Integer> positions = getFreePositions(transform);
+    int solutions = (int) Math.pow(2, positions.size());
+    int minMoves = Integer.MAX_VALUE;
+    for (int x = 0; x < solutions; x++) {
+      // Fill a solution with our free variables and solve for the remainder.
+      int[] solution = makeSolution(x, length, positions);
+      solveSolution(transform, board, solution);
+      minMoves = Math.min(minMoves, numMoves(solution));
     }
-
-    int min = Integer.MAX_VALUE;
-
-    for (int s = 0; s < n; s++) {
-      int x[] = new int[K];
-      int k = s;
-      for (int i = K - 1; i >= rank; i--) {
-        x[perm[i]] = k % 3;
-        k /= 3;
-      }
-      backSubst(K, M, x, perm, y, rank);
-      min = Math.min(min, count(x));
-    }
-
-    return min;
+    return minMoves;
   }
 
-  public void solve() throws IOException {
-    int r, c;
-    while ((r = nextInt()) != 0 && (c = nextInt()) != 0) {
-      int[] input = new int[r * c];
-      int index = 0;
-      for (int x = 0; x < r; x++) {
-        String line = nextLine();
-        for (int y = 0; y < line.length(); y++) {
-          input[index] = (line.charAt(y) == 'X') ? 1 : 0;
-          index++;
+  /**
+   * Constructs the transform matrix given the specified dimensions.
+   * The transform matrix is in the following format:
+   *   
+   *        B I 0 0 0          1 1 0 0 0          1 0 0 0 0
+   *        I B I 0 0          1 1 1 0 0          0 1 0 0 0
+   *   M =  0 I B I 0      B = 0 1 1 1 0      I = 0 0 1 0 0
+   *        0 0 I B I          0 0 1 1 1          0 0 0 1 0
+   *        0 0 0 I B          0 0 0 1 1          0 0 0 0 1
+   *      
+   * M is a rows * rows block matrix where each block is a cols * cols matrix.
+   * B is a cols * cols matrix with 1 on the diagonal and all cells adjacent to it.
+   * I is the cols * cols identity matrix.
+   * 
+   * The transform matrix M satisfies the property that Mx = b where x is a vector
+   * representing the moves to make and b is a vector representing the resulting board.
+   */
+  private static int[][] makeTransformMatrix(int rows, int cols) {
+    int length = rows * cols;
+    int[][] transform = new int[length][length];
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        int diag = cols * i + j;
+        transform[diag][diag] = 1;
+        if (i > 0) {
+          transform[diag][diag - cols] = 1;
+        }
+        if (i < rows - 1) {
+          transform[diag][diag + cols] = 1;
+        }
+        if (j > 0) {
+          transform[diag][diag - 1] = 1;
+        }
+        if (j < cols - 1) {
+          transform[diag][diag + 1] = 1;
         }
       }
+    }
+    return transform;
+  }
+  
+  /**
+   * Performs Gauss-Jordan elimination on the given transformation matrix and board.
+   * Both the matrix and board will be transformed by row operations. Matrix is assumed 
+   * to be square and consisting of only 1's and 0's.
+   */
+  private static void gaussJordan(int matrix[][], int[] board) {
+    int length = matrix.length;
+    
+    /* Iteratively constructs the row reduced form for the given matrix.
+     * Invariant: The matrix to the top and left of (row, row) is in RREF form.
+     */
+    for (int row = 0; row < length; row++) {
+      // Search for an entry in the matrix with a '1', searching down each column from (row, row).
+      int nextrow = row;
+      int col = row;
+      while (col < length && matrix[nextrow][col] != 1) {
+        nextrow++;
+        if (nextrow == length) {
+          // No entry is this column, check next one.
+          col++;
+          nextrow = row;
+        }
+      }
+      if (col == length) {
+        return;
+      }
 
-      int sol = solve(r, c, input);
-      if (sol == -1) {
-        System.out.println("Damaged billboard.");
-      } else {
-        System.out.format("You have to tap %d tiles.\n", sol);
+      // Swap our rows.
+      swapRow(matrix, row, nextrow);
+      swapElem(board, row, nextrow);
+
+      // Set every element below row in col to be 0.
+      for (int i = row + 1; i < length; i++) {
+        if (matrix[i][col] != 0) {
+          matrix[i][col] = 0;
+          for (int j = col + 1; j < length; j++)
+            matrix[i][j] = (matrix[i][j] + matrix[row][j]) % 2;
+          board[i] = (board[i] + board[row]) % 2;
+        }
       }
     }
   }
+  
+  /**
+   * Creates a partial solution given the solution number and the positions of free variables.
+   */
+  private static int[] makeSolution(int sol, int length, List<Integer> positions) {
+    int[] solution = new int[length];
+    for (int x = 0; x < positions.size(); x++) {
+      // Checks if the xth bit is set.
+      if (((sol >> x) & 1) != 0) {
+        solution[positions.get(x)] = 1;
+      }
+    }
+    return solution;
+  }
+  
+  /**
+   * Given a partially filled solution, solves the remainder of it.
+   * We want to fill solution such that transform * solution = board.
+   * 
+   * To fill the solution, we work upwards from the last row. Each step of the iteration 
+   * satisfies the invariant that the solution value for all indices after the pivot is fixed,
+   * and we use this to solve for the required value for the solution at the pivot point.
+   */
+  private static void solveSolution(int[][] transform, int[] board, int[] solution) {
+    int length = transform.length;
+    
+    for (int row = length - 1; row >= 0; row--) {
+      int index = -1;
+      int sum = 0;
+      for (int col = 0; col < length; col++) {
+        // Search for the first non-zero element in this row (pivot), then solve for it.
+        if (index == -1 && transform[row][col] != 0) {
+          index = col;
+        } else if (index != -1) {
+          // We are after the pivot so start keeping track of the desired sum.
+          sum += transform[row][col] * solution[col];
+        }
+      }
+      if (index != -1) {
+        // solution[index] must satisfy solution[index] + sum = board[index];
+        solution[index] = (sum + board[index]) % 2;
+      }
+    }
+  }
+  
+  /**
+   * Gets the number of moves specified in a move vector.
+   */
+  private static int numMoves(int[] vector) {
+    int moves = 0;
+    for (int i : vector) {
+      moves += i;
+    }
+    return moves;
+  }
+  
+  /**
+   * Finds the positions of the free variables given a row reduced transformation matrix.
+   */
+  private static List<Integer> getFreePositions(int[][] transform) {
+    List<Integer> free = new ArrayList<Integer>();
+    int row = 0;
+    for (int col = 0; col < transform[0].length; col++) {
+      if (transform[row][col] == 1) {
+        row++;
+      } else {
+        free.add(col);
+      }
+    }
+    return free;
+  }
+  
+  /**
+   * Gets the rank of a matrix in row reduced echelon form. 
+   */
+  private static int getRank(int[][] matrix) {
+    int row = 0;
+    for (int col = 0; col < matrix[0].length; col++) {
+      if (matrix[row][col] == 1) {
+        row++;
+      }
+    }
+    return row;
+  }
+  
+  /**
+   * Swaps the ith and jth elements of a vector
+   */
+  private static void swapElem(int[] vector, int i, int j) {
+    int temp = vector[i];
+    vector[i] = vector[j];
+    vector[j] = temp;
+  }
 
-  public BufferedReader br;
-  public StringTokenizer st;
-  public PrintWriter out;
+  /**
+   * Swaps the ith and jth rows of a matrix
+   */
+  private static void swapRow(int[][] matrix, int i, int j) {
+    int temp;
+    for (int k = 0; k < matrix[0].length; k++) {
+      temp = matrix[i][k];
+      matrix[i][k] = matrix[j][k];
+      matrix[j][k] = temp;
+    }
+  }
 
-  public String nextToken() throws IOException {
+  // Boilerplate IO and setup code.
+  
+  private BufferedReader br;
+  private StringTokenizer st;
+  private PrintWriter out;
+
+  private String nextToken() throws IOException {
     while (st == null || !st.hasMoreTokens()) {
       st = new StringTokenizer(br.readLine());
     }
-
     return st.nextToken();
   }
 
-  public String nextLine() throws IOException {
+  private String nextLine() throws IOException {
     return br.readLine();
   }
 
-  public int nextInt() throws IOException {
+  private int nextInt() throws IOException {
     return Integer.parseInt(nextToken());
   }
 
-  public long nextLong() throws IOException {
-    return Long.parseLong(nextToken());
-  }
-
-  public double nextDouble() throws IOException {
-    return Double.parseDouble(nextToken());
-  }
-
   public void run() throws IOException {
-    boolean oj = System.getProperty("ONLINE_JUDGE") != null;
-    oj = true;
-    br = new BufferedReader(
-        new InputStreamReader(oj ? System.in : new FileInputStream("input.txt")));
-    out = new PrintWriter(oj ? System.out : new FileOutputStream("output.txt"));
+    br = new BufferedReader(new InputStreamReader(System.in));
+    out = new PrintWriter(System.out);
     solve();
+    out.flush();
     out.close();
   }
 
